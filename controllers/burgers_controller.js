4defc904a26424d464c90aa442d9ea516ee76873
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-10-21 19:03:30 -0500

    updated hamburger.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -36,7 +36,7 @@ router.put("/api/burgers/:id", function(req, res) {
     }
   });
 });
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
   cat.delete(condition, function(result) {
     if (result.affectedRows == 0) {
